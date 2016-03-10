840f693c16736c2c3faca741adec04d69c959b75
Martin Wolfenbarger <martin@Martins-MacBook-Pro.local>
2016-03-09 16:30:30 -0800

    Using the same json schema specified in the free response version.

---
+++ test/paramSchema.test.js
@@ -0,0 +1,13 @@
+var expect = require("chai").expect;
+
+var paqChangeOfBaseMC = require("../"),
+	paqChangeOfBaseFR = require("paq-fr-change-of-base");
+
+describe("paramSchema", function() {
+	it("should be an object", function() {
+        expect(paqChangeOfBaseMC.paramSchema).to.be.an("object");
+	});
+	it("should be the same as the fr version's paramSchema", function() {
+        expect(paqChangeOfBaseMC.paramSchema).to.eql(paqChangeOfBaseFR.paramSchema);
+    });
+});
